8f570d84703e104d769b60c5a0cbf5bb4812b1cb
Leonardo Espinosa <leonardo@MacBook-Air-de-Leonardo-2.local>
2024-06-26 14:05:58 -0600

    leo_dev

---
+++ src/screens/Login/Login.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import ScreenContainer from 'src/components/layout/ScreenContainer'
+
+export default function Login() {
+    return (
+        <ScreenContainer>
+            <Text>Login</Text>
+        </ScreenContainer>
+    )
+}
