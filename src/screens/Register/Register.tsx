d068ba5f5df205f00abf36d62fd6526be9048f7f
Leonardo Espinosa <leonardo@MacBook-Air-de-Leonardo.local>
2024-06-28 21:26:49 -0600

    vistas login y register terminadas

--- src/screens/Register/Register.tsx
+++ src/screens/Register/Register.tsx
@@ -1,10 +1,76 @@
-import { View, Text } from 'react-native'
+import { View, Image, StyleSheet } from 'react-native'
 import React from 'react'
+import { NavigationProp } from '@react-navigation/native'
+import ScreenContainer from 'src/components/layout/ScreenContainer'
+import Input from 'src/components/form/Input'
+import Button from 'src/components/form/Button'
+import { Colors } from 'src/models/Colors/Colors'
+import Text from 'src/components/Texts/Text'
 
-export default function Register() {
+export default function Register({ navigation }: { navigation: NavigationProp<any> }) {
     return (
-        <View>
-            <Text>Register</Text>
-        </View>
+        <ScreenContainer style={styles.body}>
+            <Image style={styles.image} source={require('src/assets/images/logo_horizontal.png')} />
+
+            <Text style={styles.title}>
+                Registrarse
+            </Text>
+
+
+            <Input title='Nombre' requeriment='*Obligatorio' placeholder='Ingresa tu nombre' secureTextEntry />
+            <Input title='Correo' requeriment='*Obligatorio' placeholder='Correo electronico' keyboardType='email-address' />
+            <Input title='Contraseña' requeriment='*Obligatorio' placeholder='*****' secureTextEntry />
+            <Input title='Contraseña' requeriment='*Obligatorio' placeholder='*****' secureTextEntry />
+
+            <Text style={styles.notRegistedText}>Tipo de cuenta</Text>
+
+
+            <Button title='Aceptar' onPress={() => { navigation.goBack() }} style={styles.button} />
+
+
+        </ScreenContainer>
     )
 }
+
+const styles = StyleSheet.create({
+    body: {
+        paddingTop: 25,
+        paddingHorizontal: 20,
+        height: '100%',
+        paddingBottom: 20
+    },
+    title: {
+        fontSize: 34,
+        fontWeight: '900',
+        textAlign: 'center'
+    },
+    subtitle: {
+        textAlign: 'center',
+        fontSize: 20,
+        fontWeight: '600',
+        marginVertical: 60
+    },
+    notRegistedText: {
+        textAlign: 'center',
+        fontSize: 20,
+        fontWeight: '600',
+        marginBottom: 20
+    },
+    registerLink: {
+        textAlign: 'center',
+        fontSize: 18,
+        fontWeight: '600',
+        color: Colors.Blue3
+    },
+    button: {
+        backgroundColor: Colors.Blue,
+        padding: 15,
+        marginVertical: 20,
+        width: '80%'
+    },
+    image: {
+        height: 125,
+        width: '100%',
+        objectFit: 'contain',
+    }
+})
